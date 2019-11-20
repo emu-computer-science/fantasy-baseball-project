9d58ef2d5e6d4f25828e23eb155fa9ad43377307
Chad Banfield <cbanfiel@emich.edu>
2019-11-20 12:42:40 -0500

    initial player class

---
+++ Player.java
@@ -0,0 +1,36 @@
+public class Player {
+
+    String name;
+    String position;
+    int runs;
+    int hits;
+    int homeRuns;
+
+    public Player(String name, String position, int runs, int hits, int homeRuns) {
+        this.name = name;
+        this.position = position;
+        this.runs = runs;
+        this.hits = hits;
+        this.homeRuns = homeRuns;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public int getRuns() {
+        return runs;
+    }
+
+    public int getHits() {
+        return hits;
+    }
+
+    public int getHomeRuns() {
+        return homeRuns;
+    }
+}
