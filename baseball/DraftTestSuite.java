43c38e2f9022d4a94507f0fa08da5ead8dcd8fc2
arashidi21 <arashidi@emich.edu>
2019-12-04 21:14:12 -0500

    Put all tests in a test suite

---
+++ baseball/DraftTestSuite.java
@@ -0,0 +1,10 @@
+package baseball;
+
+import org.junit.runners.Suite;
+import org.junit.runner.RunWith;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({DraftTest.class, PrintTests.class})
+public class DraftTestSuite {
+
+}
